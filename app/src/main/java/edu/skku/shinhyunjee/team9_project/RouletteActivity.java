55841e4a30eb2963b86ff28f3d16ea6a670dbd1c
guswl98 <43779313+guswl98@users.noreply.github.com>
2019-06-10 22:05:25 +0900

    roulette activity update

--- app/src/main/java/edu/skku/shinhyunjee/team9_project/RouletteActivity.java
+++ app/src/main/java/edu/skku/shinhyunjee/team9_project/RouletteActivity.java
@@ -2,21 +2,71 @@ package edu.skku.shinhyunjee.team9_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class RouletteActivity extends AppCompatActivity {
+
+    Random r;
+    ImageView rouletteImage;
+
+    int degree=0, degree_old=0;
+    private static final float FACTOR = 15f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roulette);
 
+        rouletteImage = (ImageView)findViewById(R.id.rouletteImage);
         ImageButton back = (ImageButton)findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        r=new Random();
+
+        Button start=(Button)findViewById(R.id.startButton);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                degree_old=degree%360;
+                degree=r.nextInt(2000)+720;
+                RotateAnimation rotate = new RotateAnimation(degree_old,degree,RotateAnimation.RELATIVE_TO_SELF,0.5f, RotateAnimation.RELATIVE_TO_SELF,0.5f);
+                rotate.setDuration(3000); //3초 duration
+                rotate.setFillAfter(true);
+                rotate.setInterpolator(new DecelerateInterpolator());
+                rotate.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        Log.d("ROULETTE","ROULETTE is starting");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                rouletteImage.setAnimation(rotate);
+                rouletteImage.startAnimation(rotate);
+
             }
         });
     }
